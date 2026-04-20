afbb23919a270af5b110981b9578f4765d4e2912
Jaina <jainamicah@gmail.com>
2026-04-20 16:49:16 +0100

    Remove ArrayList import from DataStore

--- src/main/java/data/DataStore.java
+++ src/main/java/data/DataStore.java
@@ -4,7 +4,6 @@ import model.Room;
 import model.Sensor;
 import model.SensorReading;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
